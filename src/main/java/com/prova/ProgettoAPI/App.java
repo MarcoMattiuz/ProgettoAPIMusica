fb1c67774e6a1c7cb3541514eac234bf417b588f
MarcoMattiuz <marccs35@gmail.com>
2023-04-08 12:48:50 +0200

    aggiunto esempi di come usare le funzioni in APP

--- src/main/java/com/prova/ProgettoAPI/App.java
+++ src/main/java/com/prova/ProgettoAPI/App.java
@@ -12,7 +12,8 @@ import javax.net.ssl.HttpsURLConnection;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 
-
+import com.prova.ProgettoAPI.Topalbums.Album;
+import com.prova.ProgettoAPI.Topalbums.Album.Artist;
 import com.prova.ProgettoAPI.Toptracks.Track;
 import com.prova.ProgettoAPI.classes.ApiRequests;
 
@@ -21,10 +22,29 @@ public class App {
 	public static void main(String[] args) {
 
 		Toptracks tracks = ApiRequests.GETArtistTopTracks("TomOdell");
-
-		for ( Track t :
-				tracks.getTrack()) {
-			System.out.println(t.getName());
+		Topalbums albums = ApiRequests.GETArtistTopAlbums("PostMalone");
+		Topartists artists = ApiRequests.GETCountryTopArtist("Italy");
+		Tracks topTracks = ApiRequests.GETCountryTopTracks("Italy");
+		
+		
+		for ( com.prova.ProgettoAPI.Tracks.Track t : //attenzione che ci sono classi con lo stesso nome
+			topTracks.getTrack()) {
+		System.out.println(t.getName()+" listeners: "+t.getListeners());
 		}
+		
+//		for ( com.prova.ProgettoAPI.Topartists.Artist t : //attenzione che ci sono classi con lo stesso nome 
+//			artists.getArtist()) {
+//		System.out.println(t.getName()+" listeners: "+t.getListeners());
+//		}
+//		
+//		for ( Track t :
+//			tracks.getTrack()) {
+//		System.out.println(t.getName());
+//		}
+//		
+//		for ( Album t :
+//			albums.getAlbum()) {
+//		System.out.println(t.getName());
+//		}
 
 }}
